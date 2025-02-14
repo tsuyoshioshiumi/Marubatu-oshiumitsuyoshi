fb7eaa912f6f0e4173eb8bcb3dcefb80fe4c273e
oshiumitsuyoshi <ot104752gmail.com>
2025-02-15 08:35:45 +0900

    Initial Commit

---
+++ Marubatu/MarubatuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatuApp.swift
+//  Marubatu
+//
+//  Created by 鴛海剛 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct MarubatuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
